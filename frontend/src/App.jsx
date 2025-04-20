751259cb485e9970dd1a316f74c378662f22ecbb
Faizankhan17623 <faizankhan901152@gmail.com>
2025-04-20 20:38:51 +0530

    This is the initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
+import {Routes,Route} from 'react-router-dom'
+import  Home  from './Components/Home'
+
 function App() {
   return(
-    <div>
+    <div className='w-full bg-black'>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
       This is the testing
     </div>
   )
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>THis is made to test that everything is working</div>
+  )
+}
+
+export default Home
